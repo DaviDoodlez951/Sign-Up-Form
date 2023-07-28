4d13e8112486d0481b8417c0a0c72620030c8a55
david <davidprocter951@gmail.com>
2023-07-28 06:42:06 +0500

    completed the sign up form few touches remaining

---
+++ script.js
@@ -0,0 +1,11 @@
+// const hover = document.querySelectorAll('input[type="text"]');
+
+// // Loop through each input element and set the textContent
+// hover.forEach(inputElement => {
+//   inputElement.addEventListener('click', function()
+//   {
+//     inputElement.style = "border: 1px red solid"
+//     console.log("hwllo ")
+    
+//   })
+// });
